e1cc6b4ea6578a5253da5d9df81b455f3b143b7c
StevenPZChan <StevenPZChan@gmail.com>
2020-01-26 13:49:42 +0800

    Change to remote-theme, update favicon

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
-icons=("android-chrome-72x72.png" "apple-touch-icon.png" "browserconfig.xml" "favicon-16x16.png" "favicon-32x32.png" "favicon.ico" "mstile-150x150.png" "safari-pinned-tab.svg" "site.webmanifest")
 pages=("404.md" "categories.md" "tags.md")
-dirs=("assets" "_data" "_includes" "_layouts" "_sass")
+theme_dirs=("assets" "_includes" "_layouts" "_sass")
+custom_files=("_includes/head-custom.html" "_layouts/post.html" "_sass/*")
 
-# copy icons
-for icon in ${icons[@]}; do
-    cp -rldf $icon ../$icon
-done
+# extract icons
+unzip -uo favicon_package_v0.16 -d ../
 
 # link pages
 for page in ${pages[@]}; do
@@ -16,17 +14,16 @@ done
 # copy images
 cp -rldf images/ ../
 
-# copy directories
-for dir in ${dirs[@]}; do
+# copy customization files
+for dir in ${theme_dirs[@]}; do
     [ -d ../$dir ] && rm -rf ../$dir
-    cp -rldf $dir ../
 done
-[ -f "../navigation.yml" ] && cp -rldf ../navigation.yml ../_data/navigation.yml
+for file in ${custom_files[@]}; do
+    mkdir -p ../`dirname $file`
+    cp -rldf $file ../$file
+done
 
 # concatenate config
 cp -rf _config.yml ../_config.yml
 [ -f "../config.yml" ] && cat ../config.yml >> ../_config.yml
-
-# gemspec
-cp -rldf Gemfile jekyll-theme-so-simple.gemspec ../
 
